eace16dc91c1ba3540497948a0183c85e4337e8c
DavidM <dmeushar@marvell.com>
2020-06-08 22:02:04 +0300

    Final styling
    Change-Id: Iad35e0a8484a67b1a25d6626735d1f15d10c34b1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import React from 'react';
 import {Cards, Chart, CountryPicker} from './components'
 import styles from './App.module.css'
 import { fetchData } from './api'
+import coronaImage from './images/covid19.png'
 
 class App extends React.Component {
 
@@ -20,17 +21,18 @@ class App extends React.Component {
         this.setState({ data : fetchedData });
     }
 
-    handleCountryChange = async (country) => {
-        const fetchedData = await fetchData(country);
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
 
-        this.setState({ data: fetchedData, country: country });
-    }
+    this.setState({ data, country: country });
+  }
 
     render() {
         const { data, country } = this.state;
 
         return (
             <div className={styles.container}>
+                <img className={styles.image} src={coronaImage} alt="COVID-19" />
                 <Cards data={data}/>
                 <CountryPicker handleCountryChange={this.handleCountryChange}/>
                 <Chart data={data} country={country}/>
